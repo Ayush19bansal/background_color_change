19e6085bdd800d7689d81d354d7d88f85b8aba6f
Ayush19bansal <bansalaashu7@gmail.com>
2023-10-10 19:41:24 +0530

    addede

---
+++ script.js
@@ -0,0 +1,12 @@
+let body= document.querySelector('body');
+body.style.backgroundColor= 'red';
+
+let btn= document.querySelector("#btn");
+
+let colors= ['black', 'blue', 'cyan', 'white', 'gray', 'yellow', 'purple', 'darkblue', 'pink', 'brown', 'orange', 'darkgreen', 'violet', 'salmon'];
+
+btn.addEventListener('click', function(){
+    
+    let colorindex = parseInt(Math.random()*colors.length);
+    body.style.backgroundColor= colors[colorindex];
+})
